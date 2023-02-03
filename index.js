cd254a214ee0d5623c8865cbd438a744194e3349
avidiprb <120352429+avidiprb@users.noreply.github.com>
2023-02-02 23:46:40 -0500

    Created quote app.

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Hello");
+let quotes = ["Health is wealth", "All that glitters is not gold", "Morning doesn't show the day"]
+var n = Math.floor(Math.random() * 3);
+console.log(quotes[n]);
